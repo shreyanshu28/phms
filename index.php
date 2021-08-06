46e866dc7e0a7c48a2e22a4d167ca8d6f7e7e5d6
Kushal Gaiwala <kushalgaywala@gmail.com>
2021-08-06 10:29:13 +0530

    Added New files

--- index.php
+++ index.php
@@ -1,20 +1,91 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        hi
-        bye
-        ha
-        <?php
-        // put your code here
-        ?>
-    </body>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+    <link rel="stylesheet" href="./style.css" />
+    <title>Home</title>
+  </head>
+  <body>
+    <div class="block">
+      <div class="navbar">
+        <div class="navbar-brand">
+          <a href="#" class="navbar-item">
+            <h1 class="title is-4">Production House</h1></a
+          >
+        </div>
+        <div class="navbar-end navbar-menu is-active">
+          <a href="./index.html" class="navbar-item">Home</a>
+          <a href="./AboutUs.html" class="navbar-item">About Us</a>
+          <a href="./ContactUs.html" class="navbar-item">Contact</a>
+          <a href="./login.html" class="button is-light">Log In</a>
+          <a href="./registration.html" class="button is-info">Sign Up</a>
+        </div>
+      </div>
+    </div>
+    <section class="hero is-info">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">Title</h1>
+          <h2 class="subtitle">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas,
+            possimus.
+          </h2>
+        </div>
+      </div>
+    </section>
+    <div class="block columns">
+      <div class="card column">
+        <div class="card-content">
+          <p class="title">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum ipsum
+            maxime qui minima voluptates nemo unde? Facilis dolorum sequi
+            beatae.
+          </p>
+        </div>
+        <div class="card-footer">
+          <div class="card-footer-item">
+            <a href="">Share on <i class="fa fa-facebook"></i></a>
+          </div>
+        </div>
+      </div>
+      <div class="card column">
+        <div class="card-content">
+          <p class="title">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum ipsum
+            maxime qui minima voluptates nemo unde? Facilis dolorum sequi
+            beatae.
+          </p>
+        </div>
+        <div class="card-footer">
+          <div class="card-footer-item">
+            <a href="">Share on <i class="fa fa-facebook"></i></a>
+          </div>
+        </div>
+      </div>
+      <div class="card column">
+        <div class="card-content">
+          <p class="title">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum ipsum
+            maxime qui minima voluptates nemo unde? Facilis dolorum sequi
+            beatae.
+          </p>
+        </div>
+        <div class="card-footer">
+          <div class="card-footer-item">
+            <a href="#">Share on <i class="fa fa-facebook"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
 </html>
