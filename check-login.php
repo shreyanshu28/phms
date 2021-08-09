2a301ff79bb54fb5ee34bd4f9c1b3bfc0e0b4539
Shreyanshu Vyas <shreyanshuvyas@gmail.com>
2021-08-09 13:25:37 +0530

    Test2

--- check-login.php
+++ check-login.php
@@ -2,7 +2,7 @@
     session_start();
     if($_POST['txtUsername'] == 'admin' && $_POST['txtPassword'] == 'adminadmin'){
         $_SESSION['userKey'] = $_POST['txtUsername'];
-        header("Location: ./home.php");
+        header("Location: ./index.php");
     }else{
         header("Location: ./login.php");
     }
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -41,10 +44,18 @@
           <a href="./index.php" class="navbar-item is-active">Home</a>
           <a href="./about-us.php" class="navbar-item">About Us</a>
           <a href="./contact-us.php" class="navbar-item">Contact Us</a>
-          <a href="./login.php" class="navbar-item button mr-2 is-light"
-            >Login</a
-          >
-          <a href="./signup.php" class="navbar-item button is-info">Sign Up</a>
+          <?php
+          if(!isset($_SESSION['userKey'])) {
+              echo "<a href=./login.php class='navbar-item button mr-2 is-light'
+              >Login</a
+            >
+            <a href=./signup.php class='navbar-item button is-info'>Sign Up</a>";
+          }
+          else {
+              echo "<a href=./home.php class='navbar-item button mr-2 is-light'>Hello, $_SESSION[userKey]</a>
+                  <a href=./log-out.php class='narvbar-item button is-light'>Log out</a>";
+          }
+                    ?>
         </div>
       </div>
     </nav>
