827690ca4bfe67295b3757d5cf8b8181ee77fc07
Kushal Gaiwala <kushalgaywala@gmail.com>
2021-08-18 22:11:51 +0530

    link fixed of home of navbar in login and signup pages

--- user/signup.php
+++ user/signup.php
@@ -31,7 +31,7 @@ session_start();
 
     <div class="navbar-menu" id="navbar-main">
       <div class="navbar-end">
-        <a href="./index.php" class="navbar-item is-active">Home</a>
+        <a href="./../index.php" class="navbar-item is-active">Home</a>
         <a href="./login.php" class="navbar-item button mr-2 is-light">Login</a>
         <a href="./signup.php" class="navbar-item button is-info">Sign Up</a>
       </div>
