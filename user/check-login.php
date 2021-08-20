d8b98e5d2e9650cad3e5079ebb1777abb98a5dc1
Kushal Gaiwala <kushalgaywala@gmail.com>
2021-08-20 19:20:35 +0530

    sql transaction for check login added

--- user/check-login.php
+++ user/check-login.php
@@ -1,23 +1,21 @@
 <?php
 session_start();
+
 include "./../make-connection.php";
 
 $username = $_POST['txtUsername'];
 $password = $_POST['txtPassword'];
 
 // Transaction testing remaining
-// try {
-//     $pdo->beginTransaction();
-
-$sql = "select * from tbllogin where username = :username && password = :password";
-$stmt = $pdo->prepare($sql);
-$stmt->execute(["username" => $username, "password" => $password]);
-
-//     $pdo->commit();
-// } catch (Exception $e) {
-//     $pdo->rollBack();
-//     echo "rollback";
-// }
+try {
+    $pdo->beginTransaction();
+    $sql = "select * from tbllogin where username = :username && password = :password";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(["username" => $username, "password" => $password]);
+    $pdo->commit();
+} catch (Exception $e) {
+    $pdo->rollBack();
+}
 
 $user = $stmt->fetch();
 $userid = $user->lid;
