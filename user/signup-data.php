0e554eb659dea1545987caf1445accaec2694642
Kushal Gaiwala <kushalgaywala@gmail.com>
2021-08-21 12:46:18 +0530

    Data able to store in database from sign up page but without verification

--- user/signup-data.php
+++ user/signup-data.php
@@ -1,15 +1,69 @@
 <?php
 session_start();
 if (isset($_POST['btnSubmit'])) {
-    $_SESSION['FName'] = $_POST['txtFName'];
-    $_SESSION['MName'] = $_POST['txtMName'];
-    $_SESSION['LName'] = $_POST['txtLName'];
-    $_SESSION['ContactNo'] = $_POST['txtContactNo'];
-    $_SESSION['Email'] = $_POST['txtEmail'];
-    $_SESSION['Gender'] = $_POST['rbGender'];
-    $_SESSION['Username'] = $_POST['txtUsername'];
-    $_SESSION['Password'] = $_POST['txtPassword'];
-    $_SESSION['ConfirmPassword'] = $_POST['txtConfirmPassword'];
+    // established connection to database
+    include "./../make-connection.php";
+
+    $user = [];
+    $user['FName'] = $_POST['txtFName'];
+    $user['MName'] = $_POST['txtMName'];
+    $user['LName'] = $_POST['txtLName'];
+    $user['dob'] = $_POST['dob'];
+    // concate contactno to make 91 9283746282 as 919283746282 whole
+    $user['ContactNo'] = "${_POST['country-code']}${_POST['txtContactNo']}";
+    $user['Email'] = $_POST['txtEmail'];
+    $user['Gender'] = $_POST['rbGender'];
+    $user['Address1'] = $_POST['txtAddress1'];
+    $user['Address2'] = $_POST['txtAddress2'];
+    if (isset($_POST['txtCity'])) {
+        $user['City'] = $_POST['txtCity'];
+    } else {
+        $user['City'] = $_POST['cbCity'];
+    }
+    $user['Pincode'] = $_POST['txtPincode'];
+    $user['Landmark'] = $_POST['txtLandmark'];
+    $user['Username'] = $_POST['txtUsername'];
+    $user['Password'] = $_POST['txtPassword'];
+    $user['ConfirmPassword'] = $_POST['txtConfirmPassword'];
+
+    $sql = "INSERT INTO tblCity"
+        . "(cname, area, pincode) VALUES"
+        . "(:cname, :area, :pincode)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(['cname' => $user['City'], 'area' => $user['Address2'], 'pincode' => $user['Pincode']]);
+
+    // Making gender character uppercase
+    $gender = strtoupper($user['Gender'][0]);
+    $sql = "INSERT INTO tbluser"
+        . "(fname, mname, lname, dob, gender, contactnumber, email) VALUES"
+        . "(:fname, :mname, :lname, :dob, :gender, :contactno, :email)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([
+        'fname' => $user['FName'], 'mname' => $user['MName'], 'lname' => $user['LName'],
+        'dob' => $user['dob'], 'gender' => $gender, 'contactno' => $user['ContactNo'],
+        'email' => $user['Email']
+    ]);
+
+    $sql = "INSERT INTO tblUserAddress"
+        . "(addressline1, addressline2, landmark, cid, uid) VALUES"
+        . "(:addressline1, :addressline2, :landmark,"
+        . "(select cid from tblCity where cname = :cname && area = :area),"
+        . "(select uid from tblUser where contactnumber = :contactno))";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([
+        'addressline1' => $user['Address1'], 'addressline2' => $user['Address2'],
+        'landmark' => $user['Landmark'], 'cname' => $user['City'], 'area' => $user['Address2'], 'contactno' => $user['ContactNo']
+    ]);
+
+    $sql = "INSERT INTO tblLogin"
+        . "(username, password, uid) VALUES"
+        . "(:username, :password,"
+        . "(select uid from tblUser where contactnumber = :contactno))";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([
+        'username' => $user['Username'], 'password' => $user['Password'], 'contactno' => $user['ContactNo']
+    ]);
+
     header("Location: ./home.php");
 } else {
     echo "details remain(for testing)";
