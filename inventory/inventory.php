e88d93f3d45a46d8775fe124dcdb72c2fb2d42fc
shreyanshu28 <shreyanshuvyas@gmail.com>
2021-08-22 10:50:15 +0530

    jQuery And bootstrap for dynamic table

--- inventory/inventory.php
+++ inventory/inventory.php
@@ -8,14 +8,25 @@
       rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css"
     />
+     <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+      integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+      crossorigin="anonymous"
+    />
+    <script
+      src="https://code.jquery.com/jquery-3.6.0.slim.min.js"
+      integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI="
+      crossorigin="anonymous"
+    ></script>
     <link
       rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+      href="//cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css"
     />
     <link rel="stylesheet" href="./styles/style.css" />
     <title>Inventory</title>
   </head>
-  <body>
+  <body class="container">
     <!--Add new product
         Update new product
         Delete quantity/product
@@ -59,11 +70,31 @@
     </nav>
     <!-- Fetch products from database -->
     <div id="inventory" class="m-2 columns"></div>
-    <table class="table" id="inventoryTable" border="0">
+    <table id="myTable" class="table table-responsive-md">
       
     </table>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script
+      src="https://cdnjs.buttflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+      integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+      integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+      crossorigin="anonymous"
+    ></script>
+    <script src="//cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+    <script>
+      $(document).ready(function () {
+        $("#myTable").DataTable();
+      });
+    </script>
   </body>
   <script src="../scripts/navbar.js"></script>
   <script src="./scripts/createImage.js"></script>
-  <script src="./scripts/fetchProducts.js"></script>
+  <script src="./scripts//fetchProducts.js"></script>
 </html>
