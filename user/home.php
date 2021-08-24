37e281575491d3ea88535524335cf3313e8db3fd
Kushal Gaiwala <kushalgaywala@gmail.com>
2021-08-24 11:23:48 +0530

    created user context menu on navbar

--- user/home.php
+++ user/home.php
@@ -20,16 +20,17 @@ if (!isset($_SESSION['Username'])) {
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css" />
     <link rel="stylesheet" href="./style/style.css" />
     <title>Home</title>
 </head>
 
 <body>
-    <nav class=" navbar is-spaced" role="navigration" aria-label="main navigation">
+    <nav class="navbar is-spaced" role="navigration" aria-label="main navigation">
         <div class="navbar-brand">
-            <a href="./index.php" class="navbar-item">
-                <h1 class="title is-4">Production House</h1>
+            <a href="../index.php" class="navbar-item">
+                <h1 class="title is-4">Apricus Productions</h1>
             </a>
 
             <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-taget="navbarMain" id="navbar-burger">
@@ -41,14 +42,15 @@ if (!isset($_SESSION['Username'])) {
 
         <div class="navbar-menu" id="navbar-main">
             <div class="navbar-end">
-                <a href="./home.php" class="navbar-item is-active">Home</a>
-                <a href="./login.php" class="navbar-item button is-danger">Log Out</a>
+                <a class="navbar-item button is-light">
+                    <div class="icon is-small is-left mr-1"><i class="fa fa-user"></i></div>
+                    Hello, <?php echo $_SESSION['Username']; ?>
+                </a>
+                <a href="./log-out.php" class="navbar-item button is-danger ml-2">Logout</a>
             </div>
         </div>
     </nav>
-
     <form action="./utilities/_add-user.php" method="post" class="signup-main">
-
     </form>
 </body>
 
