91e6f677fd586356e27ed790c40ed106ee420c32
Kushal Gaiwala <kushalgaywala@gmail.com>
2021-08-24 13:12:44 +0530

    Added encryption

--- user/utilities/_check-login.php
+++ user/utilities/_check-login.php
@@ -11,7 +11,7 @@ $password = $_POST['txtPassword'];
 // TODO: Transaction testing remaining
 try {
     $pdo->beginTransaction();
-    $sql = "select * from tbllogin where username = :username && password = :password";
+    $sql = "select * from tbllogin where username = :username && password = sha(:password)";
     $stmt = $pdo->prepare($sql);
     $stmt->execute(["username" => $username, "password" => $password]);
     $pdo->commit();
